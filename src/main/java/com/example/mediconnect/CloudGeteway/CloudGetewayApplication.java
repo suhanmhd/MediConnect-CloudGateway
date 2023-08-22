c84c791ea2f8c609043a5a63ac78d2e6d00310fc
suhanmhd <suhanmuhammed4@gmail.com>
2023-08-22 07:45:52 +0530

    initial commit

---
+++ src/main/java/com/example/mediconnect/CloudGeteway/CloudGetewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.mediconnect.CloudGeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.example.mediconnect.CloudGateway")
+public class CloudGetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGetewayApplication.class, args);
+	}
+
+}
